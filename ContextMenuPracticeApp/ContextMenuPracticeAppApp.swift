8531e765738f65072bf1510f3afcf7219105c73b
Rafinad <rafinad@outlook.com>
2021-05-11 17:55:31 +0300

    Initial Commit

---
+++ ContextMenuPracticeApp/ContextMenuPracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuPracticeAppApp.swift
+//  ContextMenuPracticeApp
+//
+//  Created by Сахар Кубический on 11.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuPracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
